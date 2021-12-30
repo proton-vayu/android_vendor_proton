0c0b7fba1885585000305f5090e490f31a41ebcd
orges <me@orgesified.ml>
2021-12-30 09:37:24 +0000

    proton: add qcom extension soong config
    Signed-off-by: orges <me@orgesified.ml>

--- build/BoardConfigSoong.mk
+++ build/BoardConfigSoong.mk
@@ -1,3 +1,6 @@
 SOONG_CONFIG_NAMESPACES += aospVars
 SOONG_CONFIG_aospVars += target_init_vendor_lib
 SOONG_CONFIG_aospVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+
+SOONG_CONFIG_aospVars += qcom_um_namespace
+SOONG_CONFIG_aospVars_qcom_um_namespace := vendor/qcom/opensource/commonsys-intf # change where display_intf_headers is
